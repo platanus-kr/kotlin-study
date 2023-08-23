4904a2eb4a2a37514b7206618d2b4798227faa60
platanus-kr <platanus.kr@gmail.com>
2023-08-23 12:01:10 +0900

    lang: functions done.

---
+++ kotlin/src/main/kotlin/controlstatements/FuncPerson.kt
@@ -0,0 +1,5 @@
+package controlstatements
+
+class FuncPerson(val name: String,
+                 val age: Int) {
+}
--- kotlin/src/main/kotlin/controlstatements/functions.kt
+++ kotlin/src/main/kotlin/controlstatements/functions.kt
@@ -1,7 +1,22 @@
 package controlstatements
 
 fun main() {
+    // default argument use
+    val defaultArg = defaultArg()
+    println("${defaultArg.name}, ${defaultArg.age}")
 
+    println("-------------------------")
+
+    // named argument
+    val namedArg = namedArg(age = 30) // 이 경우 builder 패턴을 사용하지 않고도 builder의 장점을 가지게됨.
+    println("${namedArg.name}, ${namedArg.age}")
+
+    println("-------------------------")
+
+    // 가변인자 use
+    varArgs("아이유", "이지금", "이지동")
+    val arrays = arrayOf("아이유", "이지금", "이지동")
+    varArgs(*arrays)
 }
 
 // function define
@@ -22,7 +37,7 @@ public fun max2(a: Int, b: Int): Int =
     }
 
 // 리턴타입 생략 가능 (타입추론)
-public fun max3(a: Int, b: Int) =
+fun max3(a: Int, b: Int) =
     if (a > b) {
         a
     } else {
@@ -30,4 +45,19 @@ public fun max3(a: Int, b: Int) =
     }
 
 // 중괄호 생략 가능
-public fun max4(a: Int, b: Int) = if (a > b) a else b
+fun max4(a: Int, b: Int) = if (a > b) a else b
+
+
+// default argument
+fun defaultArg(name: String = "홍길동", age: Int = 20) = FuncPerson(name, age)
+
+// named argument
+fun namedArg(name: String = "아이유", age: Int) = FuncPerson(name, age)
+
+// 가변인자
+fun varArgs(vararg strs: String) {
+    for (str in strs) {
+        println(str)
+    }
+}
+
